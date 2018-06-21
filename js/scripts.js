b9984211787fc35464e2cb91856738f3bb4606b5
unknown <ad200@gazeta.pl>
2018-06-21 01:14:29 +0100

    Change html, css, js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,30 @@
-var span = $('span');
-    span.each(function(index, element) {
-});
+/* 11.4. Zadanie: Pierwsza klasa */
 
-span.each(function(index, element) {
-	if(index % 2 == 0) {
-		$(element).css('color', 'red');
-	};
-});
+function Phone(brand, price, color, origin, screan, stock, sales) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+	this.origin = origin;
+	this.screan = screan;
+	this.stock = stock;
+	this.sales = sales;
+}
 
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-    $(element).append(button);
-});
+Phone.prototype.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + ", the price is " + this.price + ", the irigin is " + this.origin + " and screan size is " + this.screan + ".");
+}
 
-$('button').click(function(){
-	alert($(this).attr('data-tmp'));
-});
+Phone.prototype.printStock = function() {
+		console.log("The phone stock is " + this.stock + " pieces.");
+}
+
+Phone.prototype.printSales = function() {
+		console.log("The phone sales was " + this.sales + " pieces last week.");
+}
+
+var iPhone6S = new Phone("Apple", 2250, "silver", "China", "7 inches", "1200", "850");
+
+iPhone6S.printInfo();
+iPhone6S.printStock();
+iPhone6S.printSales();
+	
